b1f64376beca19ba9f44d9087f963ad2c1b23374
Vishnu ES <55495318+Vishnu-es@users.noreply.github.com>
2021-09-04 23:15:43 +0530

    fetch data from TMDB

---
+++ components/Thumbnail.js
@@ -0,0 +1,48 @@
+import { ThumbUpIcon } from "@heroicons/react/outline";
+import Image from "next/image";
+import { forwardRef } from "react";
+
+const Thumbnail = forwardRef((props, ref) => {
+  const BASE_URL = "https://image.tmdb.org/t/p/original/";
+  return (
+    <div
+      ref={ref}
+      className="group cursor-pointer p-2 transition duration-200
+    ease-in transform sm:hover:scale-105 hover:z-50"
+    >
+      <Image
+        src={
+          `${BASE_URL}${
+            props.result.backdrop_path || props.result.poster_path
+          }` || `${BASE_URL}${props.result.poster_path}`
+        }
+        layout="responsive"
+        height={1080}
+        width={1920}
+      />
+      <div className="p-2">
+        <p className="truncate max-w-md">{props.result.overview}</p>
+        <h2
+          className="mt-1 text-2xl text-white
+        transition-all duration-100 ease-in-out group-hover:font-bold
+
+        "
+        >
+          {props.result.title || props.result.original_name}
+        </h2>
+        <p
+          className="flex items-center opacity-0 
+          group-hover:opacity-100 
+        "
+        >
+          {props.result.media_type && `${props.result.media_type} •`}{" "}
+          {props.result.release_date || props.result.first_air_date} •{" "}
+          <ThumbUpIcon className="h-5 mx-2" />
+          {props.result.vote_count}
+        </p>
+      </div>
+    </div>
+  );
+});
+
+export default Thumbnail;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,35 @@
 import Head from "next/head";
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+import Results from "../components/Results";
+import request from "../utils/request";
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div>
       <Head>
         <title>Stream TV and Movies Live and Online | Hulu</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>HULU Clone</h1>
+
+      {/* Header */}
+      <Header />
+      <Nav />
+      <Results results={props.results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+  const requests = await fetch(
+    `https://api.themoviedb.org/3${
+      request[genre]?.url || request.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+  return {
+    props: {
+      results: requests.results,
+    },
+  };
 }
